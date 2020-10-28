d09e013f2bea04c36c22d0eb6c496ad7becaf433
Muhammad Hammad <hammadarshad834@gmail.com>
2020-10-29 02:07:00 +0500

    feat: Assignment 1: Time to Practice: Data Binding

---
+++ Assignments/Assignment 1: Time to Practice: Data Binding/app.js
@@ -0,0 +1,9 @@
+Vue.createApp({
+    data() {
+        return {
+            name: 'Muhammad Hammad',
+            age: 21,
+            imageUrl: 'https://www.upwork.com/profile-portraits/c1F9hiCoOvr-w_59Fgqt5_1MjAklWQjB2x1qGame9DtgmednsBRLQ85U5oUffkg9Yl',
+        }
+    }
+}).mount('#assignment')
